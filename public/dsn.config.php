a0e1866d4672abf15cff39b8215898637217b0a7
cxy-js <1664008234@qq.com>
2017-12-04 22:04:18 +0800

    Update dsn.config.php

--- public/dsn.config.php
+++ public/dsn.config.php
@@ -1,12 +1,7 @@
 <?php
-//数据库地址：qdm177723556.my3w.com 
-//数据库名qdm177723556_db 
-//数据库用户名qdm177723556
-//数据库密码 1128617yecxy
-define(HOST,"qdm177723556.my3w.com");
-define(DBNAME,"qdm177723556_db");
-define(DBNAME,"qdm177723556_db");
-define(ROOT,"qdm177723556");
-define(PASSWORD,"1128617yecxy");
+define(HOST,"数据地址");
+define(DBNAME,"数据库名");
+define(ROOT,"数据库用户名");
+define(PASSWORD,"数据库密码");
 define(DSN,"mysql:host=".HOST.";dbname=".DBNAME);
 ?>
